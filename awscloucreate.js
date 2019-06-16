cf9ab75543a4d1d6099f4911ea01a5c763240741
myopenledger <cnu.srini@hotmail.com>
2019-06-16 09:48:08 -0500

    mino changes

---
+++ awscloucreate.js
@@ -0,0 +1,52 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'us-east-1'});
+var apiVersion = {apiVersion: '2010-08-01'};
+var cw = new AWS.CloudWatch(apiVersion);
+
+function putResource(){
+var params = {
+  AlarmName: 'Web_Server_CPU_Utilization',
+  ComparisonOperator: 'GreaterThanThreshold',
+  EvaluationPeriods: 1,
+  MetricName: 'CPUUtilization',
+  Namespace: 'AWS/EC2',
+  Period: 60,
+  Statistic: 'Average',
+  Threshold: 70.0,
+  ActionsEnabled: false,
+  AlarmDescription: 'Alarm when server CPU exceeds 70%',
+  Dimensions: [
+    {
+      Name: 'InstanceId',
+      Value: 'INSTANCE_ID'
+    },
+  ],
+  Unit: 'Seconds'
+};
+
+cw.putMetricAlarm(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+    describeAlarmDetails();
+  }
+});
+}
+
+function describeAlarmDetails(){
+  cw.describeAlarms({StateValue: 'INSUFFICIENT_DATA'}, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      // List the names of all current alarms in the console
+      data.MetricAlarms.forEach(function (item, index, array) {
+         console.log('AlarmName as passed in putResource fn',item.AlarmName);
+      });
+    }
+  });
+}
+
+putResource();
